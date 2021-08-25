53d86bafb90a4061d55bb9c36ebb268bf5e803f9
Asare Gideon <asaregid506@gmail.com>
2021-08-24 21:27:17 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Intro from "./components/Intro";
+import Navbar from "./components/Navbar";
+import styled, { css } from "styled-components";
+import Feature from "./components/Feature";
+import Service from "./components/Service";
+import Price from "./components/Price"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+
+const Shape = css`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
+const Container = styled.div`
+  height: 100vh;
+  position : relative;
+`;
+const Container2 = styled.div`
+  height: 80vh;
+  position : relative;
+`;
+const IntoShape = styled.div`
+  ${Shape}
+  clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
+  background-color: crimson;
+`;
+const FeatureShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 55% 0%, 33% 100%, 0 100%);
+  background-color: pink;
+`;
+const ServiceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 33% 0%, 33% 100%, 0 100%);
+  background-color: #f88497;
+`;
+
+const PriceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
+  background-color: crimson;
+`;
 
 function App() {
+  const smallScreen = window.screen.width <= 480 ? true : false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Navbar />
+        <Intro />
+        <IntoShape />
+      </Container>
+      <Container2>
+        <Feature />
+        <FeatureShape />
+      </Container2>
+       <Container>
+         <Service />
+         {!smallScreen && <ServiceShape />}
+       </Container>
+       <Container>
+         <Price />
+         <PriceShape />
+       </Container>
+       <Container>
+         <Contact />
+         <Footer />
+       </Container>
+    </>
   );
 }
 
